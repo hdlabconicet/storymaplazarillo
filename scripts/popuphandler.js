48fe95096f3b8c3e4cffe7fa766d39d6e9ef254b
HDLab~ <hdlabconicet@gmail.com>
2025-09-26 17:20:39 -0300

    commit inicial

---
+++ scripts/popuphandler.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // When hovering over the element with class "pop-display"
+    $(".pop-display").hover(function(){
+        // Show the pop-up box and its content
+        $("#popup").show();
+        $("#popup h3").show();
+    }, function(){
+        // Hide the pop-up box and its content when mouse leaves the element
+        $("#popup").hide();
+        $("#popup h3").hide();
+    });
+});
+
+function popupHandler(this, text){
+    console.log(text);
+}
